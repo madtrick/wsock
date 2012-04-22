df707581df111f90842550449811563880586f11
Farruco Sanjurjo <madtrick@gmail.com>
2012-04-22 13:36:15 +0200

    Return and error if the startline is malformed

--- src/wsock_http.erl
+++ src/wsock_http.erl
@@ -25,12 +25,16 @@
 -spec decode(Data::binary(), Type::request | response) -> #http_message{}.
 decode(Data, Type) ->
   [StartLine | Headers] = split(Data),
-  StartLineList = process_startline(StartLine, Type),
-  HeaderList = lists:foldr(fun(Element, Acc) ->
-        {match, [_Match, HeaderName, HeaderValue]} = re:run(Element, "(\.+):\s+(\.+)", [{capture, all, list}]),
-        [{string:strip(HeaderName), HeaderValue} | Acc]
-    end, [], Headers),
-  wsock_http:build(Type, StartLineList, HeaderList).
+  case process_startline(StartLine, Type) of
+    {ok, StartLineList} ->
+      HeaderList = lists:foldr(fun(Element, Acc) ->
+            {match, [_Match, HeaderName, HeaderValue]} = re:run(Element, "(\.+):\s+(\.+)", [{capture, all, list}]),
+            [{string:strip(HeaderName), HeaderValue} | Acc]
+        end, [], Headers),
+      wsock_http:build(Type, StartLineList, HeaderList);
+    {error, nomatch} ->
+      {error, malformed_request}
+  end.
 
 
 -spec build(Type::atom(), StartLine::list({atom(), string()}), Headers::list({string(), string()})) -> list(string()).
@@ -90,12 +94,18 @@ split(Data)->
 
 -spec process_startline(StartLine::binary(), Type:: request | response) -> list().
 process_startline(StartLine, request) ->
-  {match, [_, Method, RequestURI, Version]} = regexp_run("(GET)\s+([\S/])\s+HTTP\/([0-9]\.[0-9])", StartLine),
-  [{method, Method}, {resource, RequestURI}, {version, Version}];
+  process_startline(StartLine, "(GET)\s+([\S/])\s+HTTP\/([0-9]\.[0-9])", [method, resource, version]);
 
 process_startline(StartLine, response) ->
-  {match, [_, Version, Status, Reason]} = regexp_run("HTTP/([0-9]\.[0-9])\s([0-9]{3,3})\s([a-zA-z0-9 ]+)", StartLine),
-  [{version, Version}, {status, Status}, {reason, Reason}].
+  process_startline(StartLine, "HTTP/([0-9]\.[0-9])\s([0-9]{3,3})\s([a-zA-z0-9 ]+)", [version, status, reason]).
+
+-spec process_startline(StartLine::binary(), Regexp::list(), Keys::list(atom())) -> term().
+process_startline(StartLine, Regexp, Keys) ->
+  case regexp_run(Regexp, StartLine) of
+    {match, [_ | Matchs]} ->
+      {ok , lists:zip(Keys, Matchs)};
+    nomatch -> {error, nomatch}
+  end.
 
 -spec regexp_run(Regexp::list(), String::binary()) -> {match, list()}.
 regexp_run(Regexp, String) ->
--- test/spec/wsock_http_spec.erl
+++ test/spec/wsock_http_spec.erl
@@ -63,29 +63,6 @@ spec() ->
                 "\r\n"
               ]))
       end),
-    %it("should build a proper HTTP response from binary message", fun() ->
-    %      Data = <<"HTTP/1.1 205 Reset Content\r\n
-    %      Header-A: A\r\n
-    %      Header-C: dGhlIHNhbXBsZSBub25jZQ==\r\n
-    %      Header-D: D\r\n\r\n">>,
-
-    %      StatusLine = [
-    %        {version, "1.1"},
-    %        {status, "205"},
-    %        {reason, "Reset Content"}
-    %      ],
-
-    %      Headers = [
-    %        {"Header-A", "A"},
-    %        {"Header-C", "dGhlIHNhbXBsZSBub25jZQ=="},
-    %        {"Header-D", "D"}
-    %      ],
-
-    %      ExpectedResponse = #http_message{type = response, start_line = StatusLine, headers = Headers},
-    %      Response = wsock_http:from_response(Data),
-
-    %      assert_that(Response, is(ExpectedResponse))
-    %  end),
     it("should return http_message start_line values if present", fun() ->
           Message = #http_message{
             type = request,
@@ -145,7 +122,18 @@ spec() ->
                   assert_that(wsock_http:get_header_value("non-existant-header", Message), is(undefined))
 
               end),
-                  it("should return an error if the message is malformed")
+            it("should return an error if the message startline is malformed", fun() ->
+                  %Missing HTTP method
+                      Data = <<" / HTTP/1.1\r\n
+                      Host : www.example.org\r\n
+                  Upgrade : websocket\r\n
+                  Sec-WebSocket-Key : ----\r\n
+                  Header-D: D\r\n\r\n">>,
+
+                  Response = wsock_http:decode(Data, request),
+
+                  assert_that(Response, is({error, malformed_request}))
+              end )
             end),
           describe("responses", fun() ->
                 it("should return a http_message record of type response", fun() ->
@@ -166,7 +154,16 @@ spec() ->
                       assert_that(wsock_http:get_header_value("header-c", Message), is("dGhlIHNhbXBsZSBub25jZQ==")),
                       assert_that(wsock_http:get_header_value("header-d", Message), is("D"))
       end),
-                it("should return an error if the message is malformed")
+    it("should return an error if the message startline is malformed", fun() ->
+                      Data = <<"HTTP/1.1  Reset Content\r\n
+                        Header-A: A\r\n
+                        Header-C: dGhlIHNhbXBsZSBub25jZQ==\r\n
+                        Header-D: D\r\n\r\n">>,
+
+                        Response = wsock_http:decode(Data, response),
+
+                        assert_that(Response, is({error, malformed_request}))
+      end)
             end)
       end)
     end).
